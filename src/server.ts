f7d0bbad85cffac3c45630b18e5e255eb3787cc6
Nguyen Minh Phi <nmphi2710@gmail.com>
2024-11-29 15:42:31 +0700

    chore(database): configure database connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import app from './app';
+import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE_URL?.replace(
+  '<db_username>',
+  process.env.DATABASE_NAME!,
+).replace('<db_password>', process.env.DATABASE_PASSWORD!);
+
+mongoose
+  .connect(DB!)
+  .then(() => console.log('Database connection successful!'));
 
 app.listen(port, () => {
   console.log(`Listening on port:${port}...`);
