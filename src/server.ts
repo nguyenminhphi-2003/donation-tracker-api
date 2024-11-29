cc037c8a53c721c573b6f0b215d56ebeffe27712
Nguyen Minh Phi <nmphi2710@gmail.com>
2024-11-29 14:53:30 +0700

    chore: configure environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
-import express, { Express } from 'express';
 import app from './app';
 
 const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port:${process.env.PORT}...`);
+app.listen(port, () => {
+  console.log(`Listening on port:${port}...`);
 });
