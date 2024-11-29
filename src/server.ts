ff43af2a989d9804217b9692b0b9c40015b3e666
Nguyen Minh Phi <nmphi2710@gmail.com>
2024-11-29 20:58:48 +0700

    chore: change comments

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,12 @@ import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
 
-const DB = process.env.DATABASE_URL?.replace(
+const DB: string = process.env.DATABASE_URL!.replace(
   '<db_username>',
   process.env.DATABASE_NAME!,
 ).replace('<db_password>', process.env.DATABASE_PASSWORD!);
 
-mongoose
-  .connect(DB!)
-  .then(() => console.log('Database connection successful!'));
+mongoose.connect(DB).then(() => console.log('Database connection successful!'));
 
 app.listen(port, () => {
   console.log(`Listening on port:${port}...`);
