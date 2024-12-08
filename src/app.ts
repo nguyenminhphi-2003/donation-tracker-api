308b823229329f2883437df71af37b2c597649e1
nguyenminhphi-2003 <nmphi2710@gmail.com>
2024-12-08 15:17:49 +0700

    feat(user): add user routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import 'dotenv/config';
+import { MainRouter } from './routes';
 
 const app: Express = express();
 
@@ -11,5 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use('/api', MainRouter);
 
 export default app;
