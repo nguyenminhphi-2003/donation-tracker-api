09da913c56f239444f20665799537cdf8c8c940b
nguyenminhphi-2003 <nmphi2710@gmail.com>
2024-12-20 23:56:10 +0700

    feat(routes): finished user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,26 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import * as userController from '../controllers/user.controller';
 
+const filterRole = (req: Request, res: Response, next: NextFunction) => {
+  if (req.body.role) {
+    delete req.body.role;
+  }
+  next();
+};
+
 const router: Router = Router();
+
+router.use(filterRole);
 
 router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
-router.route('/:id').get(userController.getUserById);
+router
+  .route('/:id')
+  .get(userController.getUserById)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 export const UserRoutes: Router = router;
