56cf3e0839aecc19dac83b35dcd949661329d89e
nguyenminhphi-2003 <nmphi2710@gmail.com>
2024-12-20 22:58:02 +0700

    feat(interface): change creatorId type

--- src/interfaces/activity.interface.ts
+++ src/interfaces/activity.interface.ts
@@ -1,5 +1,7 @@
+import { Schema } from "mongoose";
+
 export default interface IActivity {
-  creatorId: string;
+  creatorId: Schema.Types.ObjectId;
   name: string;
   description: string;
   goalAmount: number;
