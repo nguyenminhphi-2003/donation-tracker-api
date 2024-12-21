442074374be3920a877e556290855309c990d522
nguyenminhphi-2003 <nmphi2710@gmail.com>
2024-12-21 14:54:35 +0700

    chore: update environment variable names

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,9 @@ import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
 
-const DB: string = process.env.DATABASE_URL!.replace(
-  '<db_username>',
-  process.env.DATABASE_NAME!,
-).replace('<db_password>', process.env.DATABASE_PASSWORD!);
+const DB: string = process.env
+  .DATABASE_URL!.replace('<db_username>', process.env.DATABASE_USERNAME!)
+  .replace('<db_password>', process.env.DATABASE_PASSWORD!);
 
 mongoose.connect(DB).then(() => console.log('Database connection successful!'));
 
