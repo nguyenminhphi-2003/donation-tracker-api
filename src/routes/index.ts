6db180aca909a6844dcd27f874745556297b290f
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-01-04 15:22:38 +0700

    feat(routes): add donation routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserRoutes from './user.routes';
 import ActivityRoutes from './activity.routes';
+import DonationRoutes from './donation.routes';
 
 const router: Router = Router();
 
 router.use('/users', UserRoutes);
 router.use('/activities', ActivityRoutes);
+router.use('/donations', DonationRoutes);
 
 export default router;
