c6b9b6b0d33835f8a0a3dd340744633f719ffb46
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-01-06 15:23:57 +0700

    feat(donation): add transaction handling to createDonation method

---
+++ src/controllers/donation.controller.ts
@@ -0,0 +1,89 @@
+import { NextFunction, Request, Response } from 'express';
+import Donation from '../models/donation.model';
+import Activity from '../models/activity.model';
+import User from '../models/user.model';
+import catchAsync from '../utilities/catchAsync';
+import AppError from '../utilities/appError';
+import mongoose from 'mongoose';
+import { request } from 'http';
+
+// Check if the activity and user exist
+const checkRequest = async (
+  activityId: string,
+  userId: string,
+  next: NextFunction,
+): Promise<any> => {
+  const activity = await Activity.findById(activityId);
+  if (!activity) {
+    return next(new AppError('No activity found with that ID', 404));
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+
+  return { activity, user };
+};
+
+export const getAllDonations: any = catchAsync(
+  async (req: Request, res: Response) => {
+    const donations = await Donation.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: donations.length,
+      data: {
+        donations,
+      },
+    });
+  },
+);
+
+export const createDonation: any = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { activity, user, amount } = req.body;
+    const { activity: checkedActivity } = await checkRequest(activity, user, next);
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      const donation = await Donation.create([{ ...req.body }], { session });
+
+      checkedActivity.totalDonations += amount;
+      await checkedActivity.save({ session });
+
+      await session.commitTransaction();
+      session.endSession();
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          donation,
+        },
+      });
+    } catch (err) {
+      await session.abortTransaction();
+      session.endSession();
+      return next(new AppError('Transaction failed', 500));
+    }
+  },
+);
+
+export const getDonationById: any = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const donation = await Donation.findById(req.params.id);
+
+    if (!donation) {
+      return next(new AppError('No donation found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        donation,
+      },
+    });
+  },
+);
