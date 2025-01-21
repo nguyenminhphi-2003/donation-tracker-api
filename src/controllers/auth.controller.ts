561e2e6d8dd64f36fa2c9482ca39ac2e554ba940
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-01-21 22:40:26 +0700

    feat(auth): add authorization method

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -51,26 +51,49 @@ export const login: any = catchAsync(
   },
 );
 
-export const protect: any = catchAsync( async (req: Request, res: Response, next: NextFunction) => {
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    token = req.headers.authorization.split(' ')[1];
-  }
+export const protect: any = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
 
-  if (!token) {
-    return next(new AppError('You are not logged in! Please log in to get access.', 401));
-  }
+    if (!token) {
+      return next(
+        new AppError(
+          'You are not logged in! Please log in to get access.',
+          401,
+        ),
+      );
+    }
 
-  const decoded: any = jwt.verify(token, process.env.JWT_SECRET || '');
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET || '');
 
-  const currentUser = await User.findById(decoded.id);
-  if (!currentUser) {
-    return next(new AppError('The user belonging to this token does no longer exist.', 401));
-  }
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(
+        new AppError(
+          'The user belonging to this token does no longer exist.',
+          401,
+        ),
+      );
+    }
 
-  req.user = currentUser;
-  next();
-});
+    req.user = currentUser;
+    next();
+  },
+);
+
+export const restrictTo =
+  (...roles: any[]) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+    }
+    next();
+  };
