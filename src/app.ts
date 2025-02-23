8c2b77eb43b26341a48c8e11dbb7310eeef6d9fc
nguyenminhphi-2003 <nmphi2710@gmail.com>
2025-02-23 15:38:16 +0700

    fix: correct mistype cors package

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors);
+app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
